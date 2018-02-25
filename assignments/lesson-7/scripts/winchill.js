2604fac6163097cc1cefe269506494b8f339f823
Brandylr <brandylr@byui.edu>
2018-02-24 23:46:05 -0600

    Lesson 7 assignment

---
+++ assignments/lesson-7/scripts/winchill.js
@@ -0,0 +1,8 @@
+function windChill(tempF, speed) {
+var factor = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 
+0.4275 * tempF * Math.pow(speed, 0.16);
+var digits = 2;
+var multiplier = Math.pow(10, digits);
+factor = Math.round(factor * multiplier) / multiplier;
+return factor;
+}
