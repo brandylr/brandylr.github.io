10e1a1020e6d028bd588308d2518d24e47321355
Brandylr <brandylr@byui.edu>
2018-02-25 15:21:18 -0600

    made changes to lesson 7

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,3 +1,10 @@
+
+var tempF = 90 /*parseFloat(document.getElementById(90).value);*/
+var speed = 5 /*parseFloat(document.getElementById(5).value);*/
+windChill(tempF, speed);
+var chill = windChill(tempF, speed);
+document.getElementById("windchill").innerHTML = chill;
+
 function windChill(tempF, speed) {
 var factor = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 
 0.4275 * tempF * Math.pow(speed, 0.16);
