7e2f96ce0e197c5831216c3544e673fe6b144694
Brandylr <brandylr@byui.edu>
2018-03-04 20:24:05 -0600

    lesson 8 assignments updated

--- assignments/Lesson-8/scripts/currentdate.js
+++ assignments/Lesson-8/scripts/currentdate.js
@@ -1,1 +1,9 @@
-document.getElementById("jsdate").innerHTML = "Monday, 6 April 2020";
+
+    var dayOfWeek = new Date();
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var dayOfMonth = new Date();
+    var month = new Date();
+    var m = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var year = new Date();
+
+document.getElementById("jsdate").innerHTML = days[dayOfWeek.getDay()] + "," + " " + dayOfMonth.getDate() + " " + m[month.getMonth()] + " " + year.getFullYear();
